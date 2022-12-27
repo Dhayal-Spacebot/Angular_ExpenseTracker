9f0be6382bc52dc5cd422ba4a111e2f6210b9d2c
Dhayal-Spacebot <Dhayal-Spacebot@users.noreply.github.com>
2022-12-27 10:44:23 +0000

    deploy: 69681ba6ccc68ca448bada78a5146f0bd2c456e4

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  //ROOT_URL: 'https://notes-nodejsapi.herokuapp.com'
-  //ROOT_URL: 'https://apinotes.azurewebsites.net'
-  ROOT_URL: 'http://localhost:8080'
-};
